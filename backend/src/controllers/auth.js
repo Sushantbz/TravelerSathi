5ecf485ea8ec6487622e17e6adea94e0e9919e75
SandeshGC <gcsandesh01@gmail.com>
2023-12-24 04:49:54 +0545

    fix: login page

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -4,45 +4,47 @@ const { User } = require("../models/user")
 
 // Login Controller
 const loginController = async (req, res) => {
-	console.log("login controller")
+	try {
+		// Validate request body
+		const schema = Joi.object({
+			email: Joi.string().email().required(),
+			password: Joi.string().required(),
+		})
+		const { error } = schema.validate(req.body)
+		if (error) {
+			return res
+				.status(400)
+				.json({ success: false, message: error.details[0].message })
+		}
 
-	console.log(req?.body)
+		// Find user by email
+		const user = await User.findOne({ email: req.body.email })
+		if (!user) {
+			return res.status(404).json({ success: false, message: "User not found" })
+		}
 
-	// try {
-	// 	// Validate request body
-	// 	const schema = Joi.object({
-	// 		email: Joi.string().email().required(),
-	// 		password: Joi.string().required(),
-	// 	})
-	// 	const { error } = schema.validate(req.body)
-	// 	if (error) {
-	// 		return res.status(400).json({ message: error.details[0].message })
-	// 	}
+		// Compare passwords
+		const isPasswordValid = await bcrypt.compare(
+			req.body.password,
+			user.password
+		)
+		if (!isPasswordValid) {
+			return res
+				.status(401)
+				.json({ success: false, message: "Invalid password" })
+		}
 
-	// 	// Find user by email
-	// 	const user = await User.findOne({ email: req.body.email })
-	// 	if (!user) {
-	// 		return res.status(404).json({ message: "User not found" })
-	// 	}
+		// Generate and send access token
+		// TODO: Implement access token generation and sending
 
-	// 	// Compare passwords
-	// 	const isPasswordValid = await bcrypt.compare(
-	// 		req.body.password,
-	// 		user.password
-	// 	)
-	// 	if (!isPasswordValid) {
-	// 		return res.status(401).json({ message: "Invalid password" })
-	// 	}
-
-	// 	// Generate and send access token
-	// 	// TODO: Implement access token generation and sending
-
-	// 	// Return success response
-	// 	return res.status(200).json({ message: "Login successful" })
-	// } catch (error) {
-	// 	console.error(error)
-	// 	return res.status(500).json({ message: "Internal server error" })
-	// }
+		// Return success response
+		return res.status(200).json({ success: true, message: "Login successful" })
+	} catch (error) {
+		console.error(error)
+		return res
+			.status(500)
+			.json({ success: false, message: "Internal server error" })
+	}
 }
 
 // Register Controller
@@ -66,7 +68,7 @@ const registerController = async (req, res) => {
 		if (error) {
 			return res
 				.status(400)
-				.json({ success: false, errorMessage: error.details[0].message })
+				.json({ success: false, message: error.details[0].message })
 		}
 
 		// Check if user already exists
@@ -74,7 +76,7 @@ const registerController = async (req, res) => {
 		if (existingUser) {
 			return res
 				.status(409)
-				.json({ success: false, errorMessage: "User already exists" })
+				.json({ success: false, message: "User already exists" })
 		}
 
 		// Encrypt password
@@ -100,7 +102,7 @@ const registerController = async (req, res) => {
 		console.error(error)
 		return res
 			.status(500)
-			.json({ success: false, errorMessage: "Internal server error" })
+			.json({ success: false, message: "Internal server error" })
 	}
 }
 
