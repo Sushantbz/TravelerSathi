1ba6070d7c5aad08aaf5c7281ed39ebb7e8fbae2
Sushantbz <sushantbanjade26@gmail.com>
2023-12-23 21:04:38 +0545

    Add: Three column of picture and video on landing section. Add: Hero css added

---
+++ frontend/src/shared/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import'./search-bar.css'
+import {col, Form,FormGroup} from "reactstrap";
+
+const SearchBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchBar;
