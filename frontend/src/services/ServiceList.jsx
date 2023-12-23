5c7b622648d9925ccb4e278c5c8a1f7b88f037c8
Sushantbz <sushantbanjade26@gmail.com>
2023-12-24 02:38:59 +0545

    temp

--- frontend/src/services/ServiceList.jsx
+++ frontend/src/services/ServiceList.jsx
@@ -1,12 +1,40 @@
 
-import React from 'react'
+import React from 'react';
+import ServiceCard from './ServiceCard';
+import { Col } from "reactstrap";
+
+import weatherImg from '../assets/images/weather.png'
+import guideImg from '../assets/images/guideImg'
+import customizationImg from '../assets/images/customizationImg'
+
+const servicesData = [
+  {
+    imgUrl: weatherImg,
+    title: "Calculate Weather",
+    desc: "Write anything here "
+
+  },
+  {
+    imgUrl: guideImg,
+    title: "Best Tour Guide",
+    desc: "Write anything here "
+
+  },
+  {
+    imgUrl: customizationImg,
+    title: "Customization",
+    desc: "Write anything here "
+
+  }
+
+]
 
 const ServiceList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <>
+  {
+    servicesData.map((item,index)=> <c)
+  }
+  </>
 }
 
 export default ServiceList
