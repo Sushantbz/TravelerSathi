2bae8ffa4b1f4ae4f2abcf24940e6cb319dea57b
pradeep-yednap <pradeep.yednap@gmail.com>
2023-12-24 02:01:10 +0545

    fixed merge conflict with stash

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,13 +1,47 @@
-
-
-import React from 'react'
+import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 const Register = () => {
-  return (
-    <div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your own form submission logic here
+  };
       
+  return (
+    
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className=" -mt-20 mb-4 text-2xl text-center">Join With Us!</h1>
+      <Form onSubmit={handleSubmit} className="w-full max-w-sm">
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input type="text" name="name" id="name" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="address">Address</Label>
+          <Input type="text" name="address" id="address" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="phone">Phone Number</Label>
+          <Input type="tel" name="phone" id="phone" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input type="password" name="password" id="password" required />
+        </FormGroup>
+        <Button
+          type="submit"
+          style={{backgroundColor: '#369445'}}
+          className=" custom-button w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-200"
+        >
+          Register
+        </Button>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
