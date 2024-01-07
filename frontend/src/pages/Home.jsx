43ea5fe12ff0e13ca29062f11e5345e7b891702e
pradeep-yednap <pradeep.yednap@gmail.com>
2024-01-07 15:41:28 +0545

    adding gallery section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,13 @@ import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.gif";
+import experienceImg from '../assets/images/experience.png';
 import Subtitle from "../shared/Subtitle";
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
+import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
+// import Testimonials from "../components/Testimonial/Testimonials";
 
 const Home = () => {
   return (
@@ -71,14 +74,88 @@ const Home = () => {
         <Container>
           <Row>
             <Col lg="12" className="mb-5">
-              <Subtitle subtitle={"Explorer"} />
+            <h5 className="services__subtitle">Explorer</h5>
               <h2 className="featured__tour-title">Our Featured Tours</h2>
             </Col>
-            <FeaturedTourList /> 
+            <FeaturedTourList />
           </Row>
         </Container>
       </section>
       {/*------------------ featured tour section end--------------*/}
+
+      {/*------------------ experience section start --------------*/}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="experience__content">
+              <h5 className="services__subtitle">Experience</h5>
+                <h2>
+                  {" "}
+                  With our all experience <br /> we will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.<br/>
+                  Culpa eos itaque repellat provident, reprehenderit repudiandae
+                  quod obcaecati corporis sequi nostrum ipsum.
+                </p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span> 5k+</span>
+                  <h6> Successfull Trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span> 500+</span>
+                  <h6> Regular Clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>5</span>
+                  <h6> Years experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/*------------------ experience section end --------------*/}
+
+      {/*------------------ gallery section start --------------*/}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+            <h5 className="services__subtitle">Gallery</h5>
+              <h2 className="gallery__tittle">Visit our customers tour gallery</h2>
+            </Col>
+            <Col lg='12'>
+          <MasonryImagesGallery/>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/*------------------ gallery section end --------------*/}
+
+      {/*------------------ testimonial section start --------------*/}
+      {/* <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+            <h5 className="services__subtitle">Fans Love</h5>
+            <h2 className="testimonial__title"> What our fans says about us</h2>
+            </Col>
+            <Col lg='12'><Testimonials/> </Col>
+          </Row>
+        </Container>
+      </section> */}
+      {/*------------------ testimonial section end  --------------*/}
+
 
     </>
   );
